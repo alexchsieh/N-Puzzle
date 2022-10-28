a85779919af9e4bddb0fb24be93f2bf8495c10a3
Alex Hsieh <alexchsieh@yahoo.com>
2022-10-28 16:27:52 -0700

    template copy commit

---
+++ TreeNode.py
@@ -0,0 +1,7 @@
+class TreeNode:
+
+    def __init__(self, parent, puzzle, x, y):
+        self.parent = parent
+        self.puzzle = puzzle
+        self.x = x
+        self.y = y
---
+++ main.py
@@ -0,0 +1,100 @@
+import heapq as heap
+import TreeNode
+
+# Copied this menu & default system from the example code from Project 1
+
+solved = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 0]]
+easy = [[1, 2, 3],
+        [4, 5, 6],
+        [7, 0, 8]]
+medium = [[1, 2, 0],
+          [4, 5, 3],
+          [7, 8, 6]]
+hard = [[0, 1, 2],
+        [4, 5, 3],
+        [7, 8, 6]]
+impossible = [[8, 6, 7],
+              [2, 5, 4],
+              [3, 0, 1]]
+
+
+def main():
+    puzzle_mode = input(
+        "Welcome to an 8-Puzzle Solver. Type '1' to use a default puzzle, or '2' to create your own." + '\n')
+    if puzzle_mode == "1":
+        algo(default_puzzle())
+
+    if puzzle_mode == "2":
+        print("Enter your puzzle, using a zero to represent the blank. " +
+              "Please only enter valid 8-puzzles. Enter the puzzle demilimiting " +
+              "the numbers with a space. RET only when finished." + '\n')
+        puzzle_row_one = input("Enter the first row: ")
+        puzzle_row_two = input("Enter the second row: ")
+        puzzle_row_three = input("Enter the third row: ")
+
+        puzzle_row_one = puzzle_row_one.split()
+        puzzle_row_two = puzzle_row_two.split()
+        puzzle_row_three = puzzle_row_three.split()
+
+        for i in range(0, 3):
+            puzzle_row_one[i] = int(puzzle_row_one[i])
+            puzzle_row_two[i] = int(puzzle_row_two[i])
+            puzzle_row_three[i] = int(puzzle_row_three[i])
+
+        user_puzzle = [puzzle_row_one, puzzle_row_two, puzzle_row_three]
+        algo(user_puzzle)
+
+    return
+
+
+def default_puzzle():
+    selected_difficulty = input(
+        "You wish to use a default puzzle. Please enter a desired difficulty on a scale from 0 to 4." + '\n')
+    if selected_difficulty == "0":
+        print("Difficulty of 'Trivial' selected.")
+        return solved
+    elif selected_difficulty == "1":
+        print("Difficulty of 'Easy' selected.")
+        return easy
+    elif selected_difficulty == "2":
+        print("Difficulty of 'Medium' selected.")
+        return medium
+    elif selected_difficulty == "3":
+        print("Difficulty of 'Hard' selected.")
+        return hard
+    elif selected_difficulty == "4":
+        print("Difficulty of 'Impossible' selected.")
+        return impossible
+
+
+def print_puzzle(puzzle):
+    for i in range(0, 3):
+        print(puzzle[i])
+    print('\n')
+
+
+def algo(puzzle):
+    algorithm = input(
+        "Select algorithm. (1) for Uniform Cost Search, (2) for the Misplaced Tile Heuristic, " "or (3) the Manhattan Distance Heuristic." + '\n')
+    if algorithm == "1":
+        uniform_cost_search(puzzle, 0)
+    elif algorithm == "2":
+        uniform_cost_search(puzzle, 1)
+    elif algorithm == "2":
+        uniform_cost_search(puzzle, 2)
+
+
+# def general-search(problem, QUEUEING-FUNCTION):
+#     nodes = MAKE-QUEUE(MAKE-NODE(problem.INITIAL_STATE))
+#     for i in range(0, 3):
+#         print(puzzle[i])
+#     if nodes.empty():
+#         return failure
+#     if problem.GOAL-TEST(node.STATE) == goal:
+#         return node
+#         nodes = QUEUEING-FUNCITON(nodes, EXPAND(node, problem.OPERATORS))
+
+
+main()
